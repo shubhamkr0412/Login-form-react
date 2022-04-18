72f3a8a289b21f6208619176503b72754c440ba1
Shubham Kumar <shubham35godda@gmail.com>
2022-04-18 09:11:56 +0530

    installed react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,37 @@
-
-import './App.css';
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Homepage } from "./components/Homepage";
+import { Login } from "./components/Login";
+import { Settings } from "./components/Settings";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { PrivateRoutes } from "./components/PrivateRoutes";
 
 function App() {
   return (
-    <main className='app'>
-      <h1 style={{color:"red" , textAlign:"center"}}>Login Form</h1>
-    </main>
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoutes>
+              <Settings />
+            </PrivateRoutes>
+          }
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoutes>
+              <Dashboard />
+            </PrivateRoutes>
+          }
+        ></Route>
+      </Routes>
+    </div>
   );
 }
 
